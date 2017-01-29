4794c8b90aa5b26725699381ebd28fce64a012eb
Cristian Lozano <criscalovis@gmail.com>
2017-01-29 18:38:02 -0500

    css fix

--- src/components/login/ServiceCounter.tsx
+++ src/components/login/ServiceCounter.tsx
@@ -36,15 +36,15 @@ export default class ServiceCounter extends React.Component<IServiceCounter, {}>
   getDisplayComponent = () => {
     return (
       <div className={styles.display}>
-        <div className={styles.displayItem}>
+        <div>
           <div>{this.state.hours}</div>
           <div>Hours</div>
         </div>
-        <div className={styles.displayItem}>
+        <div>
           <div>{this.state.minutes}</div>
           <div>Minutes</div>
         </div>
-        <div className={styles.displayItem}>
+        <div>
           <div>{this.state.seconds}</div>
           <div>Seconds</div>
         </div>
