483cb621c138153adafdf672260ab032033f1e92
Ignusmart <ignusmart@gmail.com>
2017-01-29 21:23:13 -0500

    Fix SessionCounter Layout

--- src/components/login/SessionCounter.tsx
+++ src/components/login/SessionCounter.tsx
@@ -46,11 +46,11 @@ export default class SessionCounter extends React.Component<ISessionCounterProps
           <div>{this.state.hours}</div>
           <div>Hours</div>
         </div>
-        <div>
+        <div className={styles.displayItem}>
           <div>{this.state.minutes}</div>
           <div>Minutes</div>
         </div>
-        <div>
+        <div className={styles.displayItem}>
           <div>{this.state.seconds}</div>
           <div>Seconds</div>
         </div>
