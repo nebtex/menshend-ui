d22fc6aee4ed6a93a7e71a9f234aeceb463b4ec7
Ignusmart <ignusmart@gmail.com>
2017-02-16 12:59:29 -0500

    basis structure of .spec.tsx file for EditModal

---
+++ src/components/services/EditModal/__tests__/EditModal.spec.tsx
@@ -0,0 +1,23 @@
+/// <reference types='jest' />
+
+import * as React from 'react';
+import  EditModal from '../EditModal';
+import  Cases from './EditModal.cases';
+import { shallow, mount, render } from 'enzyme';
+
+describe('EditModal', () => {
+  let editModal:any;
+
+  describe('default', () => {
+
+    const testProps = Cases['default'];
+
+    beforeEach(() => {
+      editModal = mount(<EditModal {...testProps} />);
+    });
+
+    it('should render correctly', () => {
+     
+    });
+  });
+});
