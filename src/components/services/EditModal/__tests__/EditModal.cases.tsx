c48bdfb8dd460b429da1507da999fe2119c54ae4
Ignusmart <ignusmart@gmail.com>
2017-02-18 20:20:12 -0500

    Roles tabPane tests

--- src/components/services/EditModal/__tests__/EditModal.cases.tsx
+++ src/components/services/EditModal/__tests__/EditModal.cases.tsx
@@ -3,7 +3,7 @@ let testLogo = require('../../../../assets/testLogo.svg');
 
 let Cases : {[id:string]:IEditModalProps} = {};
 
-Cases['No service'] = {
+Cases['Default'] = {
   isOpen: true,
   toggle: function(){},
   roles: ['role 1', 'role 2', 'role 3', 'role 4', 'role 5', 'role 6', 'role 7', 'role 8']
--- src/components/services/EditModal/__tests__/EditModal.spec.tsx
+++ src/components/services/EditModal/__tests__/EditModal.spec.tsx
@@ -8,29 +8,72 @@ import { shallow, mount, render } from 'enzyme';
 describe('EditModal', () => {
   let editModal:any;
 
-  describe('No service', () => {
+  describe('Default', () => {
 
-    const testProps = Cases['No service'];
+    const testProps = Cases['Default'];
 
-    beforeEach(() => {
-      editModal = mount(<EditModal {...testProps} />);
-    });
+    describe('Roles TabPane' ,() => {
+      const testRole = 'role 1';
+     
+      beforeEach(() => {
+        editModal = mount(<EditModal {...testProps} />);
+      });
 
-    it('should render correctly', () => {
-      //@TODO: Check if default values are rendered correctly
+      it('should move a role from available roles to service roles when double click over role is triggered', () => {
+
+        let availableRoles = editModal.state().availableRoles,
+            serviceRoles = editModal.state().serviceRoles;
+        
+        expect(availableRoles.includes(testRole)).toEqual(true);
+        expect(serviceRoles.includes(testRole)).toEqual(false);
+
+        editModal.instance().availableRolesOnDoubleClick(testRole);
+        expect(availableRoles.includes(testRole)).toEqual(false);
+        expect(serviceRoles.includes(testRole)).toEqual(true);
+      });
+
+      it(`should move a role from service roles to available roles 
+          when double click over role is triggered and role exists in props`, () => {
+
+        let availableRoles = editModal.state().availableRoles,
+            serviceRoles = editModal.state().serviceRoles;
+
+        editModal.instance().availableRolesOnDoubleClick(testRole);
+        expect(availableRoles.includes(testRole)).toEqual(false);
+        expect(serviceRoles.includes(testRole)).toEqual(true);
+
+        editModal.instance().serviceRolesOnDoubleClick(testRole);
+        expect(serviceRoles.includes(testRole)).toEqual(false);
+        expect(availableRoles.includes(testRole)).toEqual(true);
+        expect(editModal.props().roles.includes(testRole)).toEqual(true);
+      });
+
+      it(`should remove a role from service roles and does not move that role to available roles 
+          when double click over role is triggered and role does not exist in props`, () => {
+        
+        const newTestRole = 'newTestRole';
+        let availableRoles = editModal.state().availableRoles,
+            serviceRoles = editModal.state().serviceRoles;
+
+        editModal.instance().serviceRoleOnAddition(newTestRole);
+        expect(serviceRoles.includes(newTestRole)).toEqual(true);
+        
+        editModal.instance().serviceRolesOnDoubleClick(newTestRole);
+        expect(serviceRoles.includes(newTestRole)).toEqual(false);
+        expect(availableRoles.includes(newTestRole)).toEqual(false);
+      });
     });
   });
 
   describe('Service', () => {
 
-    const testProps = Cases['No service'];
+    const testProps = Cases['Service'];
 
     beforeEach(() => {
       editModal = mount(<EditModal {...testProps} />);
     });
 
-    it('should render correctly', () => {
-      //@TODO: Check if the service data is rendered correctly
+    it('should have the service values as state', () => {
     });
   });
 });
