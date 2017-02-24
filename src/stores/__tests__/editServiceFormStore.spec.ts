c44ef0ed39aa9de55f1ff52b17b3a7eeb8aa6718
Ignusmart <ignusmart@gmail.com>
2017-02-24 11:40:42 -0500

    states -> stores folder

---
+++ src/stores/__tests__/editServiceFormStore.spec.ts
@@ -0,0 +1,13 @@
+/// <reference types='jest' />
+
+describe('editServiceFormStore', () => {
+  describe('Default', () => {
+    it('should set allRoles from read all the current available services roles', () => {
+      let mock = jest.fn();
+    });
+
+    it('should check the serviceRoles and remove all the roles in the roles map that are not defined in serviceRoles before the API save call', () => {
+      
+    });
+  });
+});
