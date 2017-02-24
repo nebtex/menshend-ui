cc8a094626ac3577e5b4287da6be0608bcb3098b
Ignusmart <ignusmart@gmail.com>
2017-02-24 14:16:40 -0500

    login and serviceList stores basic tests structure

---
+++ src/stores/__tests__/loginStore.spec.ts
@@ -0,0 +1,10 @@
+/// <reference types='jest' />
+
+describe('editServiceFormStore', () => {
+  describe('Default', () => {
+    it('should ...', () => {
+      let mock = jest.fn();
+      
+    });
+  });
+});
---
+++ src/stores/__tests__/serviceListStore.spec.ts
@@ -0,0 +1,10 @@
+/// <reference types='jest' />
+
+describe('editServiceFormStore', () => {
+  describe('Default', () => {
+    it('should return all the available roles from services', () => {
+      let mock = jest.fn();
+      
+    });
+  });
+});
