dc6ae47b70eab937af7a89075e60f330744ecdd0
Ignusmart <ignusmart@gmail.com>
2017-03-01 14:00:54 -0500

    2 spaces for identation

--- src/components/modals/FullModal.tsx
+++ src/components/modals/FullModal.tsx
@@ -5,10 +5,10 @@ import { Navbar, NavbarToggler, NavbarBrand, Collapse, Form, Nav, NavItem, NavLi
 let styles = require('./FullModal.scss');
 
 export interface IFullModalProps {
-    className?: string
-    isOpen?: boolean
-    toggle?: { (): void }
-    title?: string
+	className?: string
+	isOpen?: boolean
+	toggle?: { (): void }
+	title?: string
 }
 
 export interface IFullModalState { }
@@ -17,31 +17,31 @@ export interface IFullModalState { }
  * use this component when you need a full size modal
  */
 export default class FullModal extends React.Component<IFullModalProps, IFullModalState>{
-    handleEscape = (event: any) => {
-        if (event.keyCode == 27) {
-            if (this.props.isOpen) {
-                this.props.toggle()
-            }
-        }
-    }
-    render() {
-        if (!this.props.isOpen) return null;
-        return (
-            <div onKeyPress={this.handleEscape} className={styles.modalContainer}>
-                <Navbar fixed="top" className={classnames(styles.navbar, this.props.className)} light>
-                    <NavbarBrand href="#">{this.props.title}</NavbarBrand>
-                    <Collapse isOpen={true} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <Button color="danger" outline onClick={this.props.toggle}>Close</Button>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-                <div className={styles.modalContent}>
-                    {this.props.children}
-                </div>
-            </div>
-        )
-    }
+	handleEscape = (event: any) => {
+		if (event.keyCode == 27) {
+			if (this.props.isOpen) {
+				this.props.toggle()
+			}
+		}
+	}
+	render() {
+		if (!this.props.isOpen) return null;
+		return (
+			<div onKeyPress={this.handleEscape} className={styles.modalContainer}>
+				<Navbar fixed="top" className={classnames(styles.navbar, this.props.className)} light>
+					<NavbarBrand href="#">{this.props.title}</NavbarBrand>
+					<Collapse isOpen={true} navbar>
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<Button color="danger" outline onClick={this.props.toggle}>Close</Button>
+							</NavItem>
+						</Nav>
+					</Collapse>
+				</Navbar>
+				<div className={styles.modalContent}>
+					{this.props.children}
+				</div>
+			</div>
+		)
+	}
 }
