354cce29fe96d9e34f384cbc1727f37b0c38d220
Cristian Lozano <criscalovis@gmail.com>
2017-03-01 08:42:50 -0500

    add full modal

--- src/components/modals/FullModal.tsx
+++ src/components/modals/FullModal.tsx
@@ -26,29 +26,23 @@ export default class FullModal extends React.Component<IFullModalProps, IFullMod
         }
     }
     render() {
-
+        if (!this.props.isOpen) return null;
         return (
-            <ReactCSSTransitionGroup
-                transitionName="example"
-                transitionEnterTimeout={500}
-                transitionLeaveTimeout={300}>
-                {<div onKeyPress={this.handleEscape} >
-                    <Navbar fixed="top" className={classnames(styles.navbar, this.props.className)} light>
-                        <NavbarBrand href="#">{this.props.title}</NavbarBrand>
-                        <Collapse isOpen={true} navbar>
-                            <Nav className="ml-auto" navbar>
-                                <NavItem>
-                                    <Button color="danger" outline onClick={this.props.toggle}>Close</Button>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </Navbar>
-                    <div className={styles.modalContent}>
-                        {this.props.children}
-                    </div>
-                </div>}
-            </ReactCSSTransitionGroup>
-
+            <div onKeyPress={this.handleEscape} >
+                <Navbar fixed="top" className={classnames(styles.navbar, this.props.className)} light>
+                    <NavbarBrand href="#">{this.props.title}</NavbarBrand>
+                    <Collapse isOpen={true} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <Button color="danger" outline onClick={this.props.toggle}>Close</Button>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+                <div className={styles.modalContent}>
+                    {this.props.children}
+                </div>
+            </div>
         )
     }
 }
