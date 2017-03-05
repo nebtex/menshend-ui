5a86216b5658aa41251151853eefa5cef1b8ee41
Ignusmart <ignusmart@gmail.com>
2017-03-05 17:40:51 -0500

    actions

--- src/stores/editServiceFormStore.ts
+++ src/stores/editServiceFormStore.ts
@@ -2,26 +2,16 @@ import { observable, action, ObservableMap, toJS, IObservableArray } from 'mobx'
 import ServiceListStore from './serviceListStore';
 import { AdminApi, AdminService, AdminServiceCache, AdminServiceCors } from '../api/api';
 
-interface IServiceRole {
-  luaScript: string;
-  impersonateWithinRole: boolean;
-  proxy: boolean;
-  isActive: boolean;
-}
-
 export default class EditServiceFormStore {
   adminApi: AdminApi = new AdminApi();
 
   @observable roleId: string = ''
-  @observable subDomain: string = ''
   @observable logo: string = ''
   @observable name: string = ''
   @observable shortDescription: string = ''
   @observable longDescription: string = ''
   @observable longDescriptionUrl: string = ''
   @observable proxyCode: string = '__=== your code here ==='
-
-  // @TODO: Set the default values of nexts vars
   @observable csrf: boolean
   @observable impersonateWithinRole: boolean
   @observable proxyCodeLanguage: number
@@ -31,124 +21,74 @@ export default class EditServiceFormStore {
   @observable cors: AdminServiceCors
   @observable secretPaths: string[]
 
-  // roles that will be sent to backend, remove all the roles in the roles map that are not defined in serviceRoles
-  @observable roles: ObservableMap<IServiceRole> = observable.map<IServiceRole>({})
-  
+  @action updateRoleId = (roleId:string) => {
+    this.roleId = roleId
+  }
+
+  @action updateLogo = (logo:string) => {
+    this.logo = logo
+  }
+
+  @action updateName = (name:string) => {
+    this.name = name
+  }
+
+  @action updateShortDescription = (shortDescription:string) => {
+    this.shortDescription = shortDescription
+  }
+
+  @action updateLongDescription = (longDescription:string) => {
+    this.longDescription = longDescription
+  }
+
+  @action updateLongDescriptionUrl = (longDescriptionUrl:string) => {
+    this.longDescriptionUrl = longDescriptionUrl
+  }
+
+  @action updateProxyCode = (proxyCode:string) => {
+    this.proxyCode = proxyCode
+  }
+
+  @action updateCsrf = (csrf:boolean) => {
+    this.csrf = csrf
+  }
+
+  @action updateImpersonateWithinRole = (impersonateWithinRole:boolean) => {
+    this.impersonateWithinRole = impersonateWithinRole
+  }
+
+  @action updateProxyCodeLanguage = (proxyCodeLanguage:number) => {
+    this.proxyCodeLanguage = proxyCodeLanguage
+  }
+
+  @action updateIsActive = (isActive:boolean) => {
+    this.isActive = isActive
+  }
+
+  @action updateStrategy = (strategy:number) => {
+    this.strategy = strategy
+  }
+
+  //@TODO: How to manage this
+  @action updateCache = () => {
+
+  }
+
+  //@TODO: How to manage this
+  @action updateCors = () => {
+
+  }
+
+  //@TODO: How to manage this
+  @action updateSecretPaths = () => {
+
+  }
+
   //service roles this is useful for maintain a track of the roles that will be send to the backend
   @observable serviceRoles: IObservableArray<string> = observable.array<string>([])
 
   //all the roles this come from read all the current available services
   @observable allRoles: IObservableArray<string> = observable.array<string>([])
-
-  @action updateLuaScript = (value: string, role: string) => {
-    if (!this.roles.has(role)) return;
-    let txRole = this.roles.get(role)
-    txRole.luaScript = value
-    this.roles.set(role, txRole);
-  }
-
-  @action updateImpersonateWithinRole = (value: boolean, role: string) => {
-    if (!this.roles.has(role)) return;
-    let txRole = this.roles.get(role)
-    txRole.impersonateWithinRole = value
-    this.roles.set(role, txRole);
-  }
-
-  @action updateProxy = (value: boolean, role: string) => {
-    if (!this.roles.has(role)) return;
-    let txRole = this.roles.get(role)
-    txRole.proxy = value
-    this.roles.set(role, txRole);
-  }
-
-  @action updateIsActive = (value: boolean, role: string) => {
-    if (!this.roles.has(role)) return;
-    let txRole = this.roles.get(role)
-    txRole.isActive = value
-    this.roles.set(role, txRole);
-  }
-
-  @action updateSubDomain = (value: string) => {
-    this.subDomain = value;
-  }
-
-  @action updateName = (value: string) => {
-    this.name = value;
-  }
-
-  @action updateLogo = (value: string) => {
-    this.logo = value;
-  }
-
-  @action updateShortDescription = (value: string) => {
-    this.shortDescription = value;
-  }
-
-  @action updateLongDescription = (value: string) => {
-    this.longDescription = value;
-  }
-
-  @action updateLongDescriptionUrl = (value: string) => {
-    this.longDescriptionUrl = value;
-  }
-
-  @action addRole = (role: string) => {
-    //if role already exist leave
-    if (this.roles.has(role)) return;
-
-    let NewRole: IServiceRole = {
-      luaScript: "__==== your code here ====",
-      impersonateWithinRole: false,
-      proxy: true,
-      isActive: true,
-    }
-    this.roles.set(role, NewRole);
-  }
-
-  @action deleteRole = (role: string) => {
-    this.roles.delete(role)
-  }
-
-  @action addServiceRole = (role:string) => {
-    if(this.serviceRoles.indexOf(role)>-1) return;
-    this.serviceRoles.push(role);
-    this.serviceRoles.sort();
-    this.addRole(role);
-  }
-
-  @action deleteServiceRole = (role:string) => {
-    this.serviceRoles.remove(role);
-  }
-
-  @action addToAllRoles = (role:string) => {
-    if(this.allRoles.indexOf(role)>-1) return;
-    this.allRoles.push(role);
-    this.allRoles.sort();
-  }
-
-  @action deleteFromAllRoles = (role:string) => {
-    this.allRoles.remove(role);
-  }
-
-  @action moveRoleFromAllToService = (role:string) => {
-    this.deleteFromAllRoles(role);
-    this.addServiceRole(role);
-  }
-
-  @action moveRoleFromServiceToAll = (role:string) => {
-    this.deleteServiceRole(role);
-    this.addToAllRoles(role);
-  }
-
-  @action save = () => {
-    let rolesToDelete: string[] = []
-    
-    // clean the roles map before api call
-    this.roles.forEach((role:IServiceRole, key:string) => {
-      if(this.serviceRoles.indexOf(key) === -1)
-        rolesToDelete.push(key)
-    });
-  }
 
   @action apiGetService = (serviceId:string) => {
     return this.adminApi.adminGetService({id:serviceId}).then((service:AdminService) => {
