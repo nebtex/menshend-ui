3eb784dbb4e7f15be9a4c4862c359437aef0a18c
Ignusmart <ignusmart@gmail.com>
2017-03-05 17:43:16 -0500

    roles tests removed

--- src/stores/__tests__/editServiceFormStore.spec.ts
+++ src/stores/__tests__/editServiceFormStore.spec.ts
@@ -85,22 +85,6 @@ describe('editServiceFormStore', () => {
 
   });
 
-  it('should create a new role in service map if a role is moved from AllRoles to a  serviceRoles, and the role does not exist', () => {
-    editServiceFormStore.addToAllRoles('my_test_role');
-    editServiceFormStore.moveRoleFromAllToService('my_test_role');
-    expect(editServiceFormStore.allRoles.indexOf('my_test_role')).toEqual(-1);
-    expect(editServiceFormStore.serviceRoles.indexOf('my_test_role')).toBeGreaterThan(-1);
-    expect(editServiceFormStore.roles.has('my_test_role')).toEqual(true);
-  });
-
-  it('should not change the service map if a existing serviceRoles is move to AllRoles', () => {
-    editServiceFormStore.addServiceRole('my_test_role');
-    editServiceFormStore.moveRoleFromServiceToAll('my_test_role');
-    expect(editServiceFormStore.allRoles.indexOf('my_test_role')).toBeGreaterThan(-1);
-    expect(editServiceFormStore.serviceRoles.indexOf('my_test_role')).toEqual(-1);
-    expect(editServiceFormStore.roles.has('my_test_role')).toEqual(true);
-  });
-
   it('should check the serviceRoles and remove all the roles in the roles map that are not defined in serviceRoles before the API save call', () => {
 
   });
