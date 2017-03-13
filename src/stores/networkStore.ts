6b2abbf506bc855f9ccdb1f7c134d468218eead8
Ignusmart <ignusmart@gmail.com>
2017-03-12 21:39:32 -0500

    adjustments

--- src/stores/networkStore.ts
+++ src/stores/networkStore.ts
@@ -1,12 +1,7 @@
 import { observable, action, ObservableMap, toJS, IObservableArray } from 'mobx';
 
 class NetworkStore {
-  @observable lastResponseCode: string = ''
   @observable lastResponseMessage: string = ''
-
-  @action updateLastResponseCode = (value:string) => {
-    this.lastResponseCode = value;
-  }
 
   @action updateLastResponseMessage = (value:string) => {
     this.lastResponseMessage = value;
