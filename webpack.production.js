16695910a3e437a286ec369770e2d0f1f49708c2
Ignusmart <ignusmart@gmail.com>
2017-03-28 15:57:58 -0500

    Minification

--- webpack.production.js
+++ webpack.production.js
@@ -1,15 +1,26 @@
 const webpack = require("webpack");
 const path = require("path");
 const webpackMerge = require('webpack-merge');
-const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 const commonConfig = require('./webpack.common.js');
+const BabiliPlugin = require('babili-webpack-plugin');
+const ClosureCompilerPlugin = require('webpack-closure-compiler');
 
 const config = webpackMerge(commonConfig, {
   devtool: "source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
-  }
+  },
+  plugins: [
+    new ClosureCompilerPlugin({
+      compiler: {
+        language_in: 'ECMASCRIPT6',
+        language_out: 'ECMASCRIPT5',
+        compilation_level: 'ADVANCED'
+      },
+      concurrency: 3,
+    })
+  ]
 });
 
 module.exports = config;
