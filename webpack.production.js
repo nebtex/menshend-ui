cccd9c5a32e1fd0e116899aa7f264c33890104af
Ignusmart <ignusmart@gmail.com>
2017-03-31 14:27:07 -0500

    Reactstrap external

--- webpack.production.js
+++ webpack.production.js
@@ -3,6 +3,7 @@ const path = require("path");
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const ClosureCompilerPlugin = require('webpack-closure-compiler');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config = webpackMerge(commonConfig, {
   devtool: "source-map",
@@ -10,19 +11,29 @@ const config = webpackMerge(commonConfig, {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
-  plugins: [
-    new ClosureCompilerPlugin({
-      compiler: {
-        language_in: 'ECMASCRIPT6',
-        language_out: 'ECMASCRIPT5',
-        compilation_level: 'ADVANCED'
-      },
-      concurrency: 3,
-    })
-  ],
+  // plugins: [
+  //   new ClosureCompilerPlugin({
+  //     compiler: {
+  //       language_in: 'ECMASCRIPT6',
+  //       language_out: 'ECMASCRIPT5',
+  //       compilation_level: 'ADVANCED'
+  //     },
+  //     concurrency: 3,
+  //   })
+  // ],
   externals: {
-    reactstrap: 'reactstrap'
-  }
+    'reactstrap': {
+      'commonjs': 'reactstrap',
+      'commonjs2': 'reactstrap',
+      'amd': 'reactstrap',
+      'root': 'Reactstrap'
+    }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'index.html' }
+    ])
+  ]
 });
 
 module.exports = config;
