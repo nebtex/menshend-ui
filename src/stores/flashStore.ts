db4e6b2f033235645776c37fa5f7d3d14b5742de
Ignusmart <ignusmart@gmail.com>
2017-04-01 15:41:48 -0500

    flashStore

---
+++ src/stores/flashStore.ts
@@ -0,0 +1,33 @@
+import { observable, action, IObservableArray } from 'mobx';
+import { FlashesApi, Flash } from '../api/api';
+import networkStore from './networkStore';
+
+const flashesApi = new FlashesApi();
+
+class FlastStore {
+  @observable flashes:IObservableArray<string> = observable.array<string>([]);
+  constructor() {
+    const flash:Flash = localStorage.getItem('flash') ? JSON.parse(localStorage.getItem('flash')) : {flashes:[]};
+    flash.flashes.forEach(flash => {
+      this.flashes.push(flash);
+    });
+  }
+
+  @action load() {
+    networkStore.addPendingRequest();
+    flashesApi.getFlashes().then(flash => {
+      flash.flashes.forEach(flash => {
+        this.flashes.push(flash);
+      });
+      networkStore.updateLastResponse({message:'OK', statusCode: 200});
+      networkStore.removePendingRequest();
+    }).catch((response:Response) => {
+      networkStore.updateLastResponse({message:response.statusText, statusCode: response.status});
+      networkStore.removePendingRequest();
+    });
+  }
+}
+
+const flashStore = new FlastStore();
+
+export default flashStore;
