5d99cf89f7c74a0b2ddb8eaff44803a8791d6357
Ignusmart <ignusmart@gmail.com>
2017-04-11 23:48:34 -0500

    initial scrollSpy approach

--- scripts/fixTypes.sh
+++ scripts/fixTypes.sh
@@ -6,3 +6,5 @@ rm -rf node_modules/@types/fetch-intercept
 ln -s `pwd`/types/fetch-intercept `pwd`/node_modules/@types/fetch-intercept
 rm -rf node_modules/@types/mobx-router
 ln -s `pwd`/types/mobx-router `pwd`/node_modules/@types/mobx-router
+rm -rf node_modules/@types/react-scrollspy
+ln -s `pwd`/types/react-scrollspy `pwd`/node_modules/@types/react-scrollspy
