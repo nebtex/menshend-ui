5cddb53de2b650b816c7898f8ae9ab73785dda25
Cristian Lozano <criloz@nebtex.com>
2017-04-23 22:15:32 -0500

    fix github login endpoint

--- src/components/login/LoginForm/GithubLogin.tsx
+++ src/components/login/LoginForm/GithubLogin.tsx
@@ -14,7 +14,7 @@ export default class GithubLogin extends React.Component<IGithubLoginProps, {}>{
   render(){
     let error = this.props.error;
     return (
-      <Form action={`/ui/auth/github/${clientServiceStore.currentDomain?clientServiceStore.currentDomain:""}`} method="get">
+      <Form action={`/ui/auth/github${clientServiceStore.currentDomain?"/"+clientServiceStore.currentDomain:""}`} method="get">
         <Button style={{width:'100%'}} outline={error} color={error ? "danger" : "secondary"}>
           <i className="fa fa-github fa-lg"/> | Github Login
         </Button>
