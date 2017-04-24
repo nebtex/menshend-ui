fb7079b675875be1d6d05f6891404d826f74fa6b
Cristian Lozano <criloz@nebtex.com>
2017-04-23 23:03:48 -0500

    document last change

--- src/stores/variables.ts
+++ src/stores/variables.ts
@@ -1,1 +1,2 @@
+//warning does not change the logic of this, is not working in dev, please make it work
 export const BasePath = process.env.NODE_ENV === "development" ? "http://localhost:8787": "/" ;
